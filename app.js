4bce620ce0dbd0e6dbf3ceff6c937122bb40156c
naman-mehta2001 <f20190981@pilani.bits-pilani.ac.in>
2021-01-01 15:36:16 +0530

    taking input and display

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-var userName=prompt("Enter your username");
-var welcomemessage = alert("Welcome"+userName)
+var btnTranslate = document.querySelector("#btn-translate");
+var txtInput = document.querySelector("#txt-input");
+function clickHandler() {
+    console.log("clicked");
+    console.log("Input", txtInput.value)
+};
+btnTranslate.addEventListener("click", clickHandler)
