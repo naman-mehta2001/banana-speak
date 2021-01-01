7963ea7625ca56c8a0ce49060a16e818be18c363
naman-mehta2001 <f20190981@pilani.bits-pilani.ac.in>
2021-01-01 16:14:14 +0530

    input output processing

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
+var outputDiv = document.querySelector("#output");
 function clickHandler() {
-    console.log("clicked");
-    console.log("Input", txtInput.value)
+    outputDiv.innerText="asasasasasasasasasasas" + txtInput.value
 };
-btnTranslate.addEventListener("click", clickHandler)
+btnTranslate.addEventListener("click", clickHandler);
+
